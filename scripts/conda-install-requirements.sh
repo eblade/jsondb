49e1253f680320b2f57913d0f6150af5912952dc
Johan Egneblad <johan@egneblad.se>
2016-09-08 22:48:11 +0100

    Switch sortedcontainers for blist fork in conda-install-requirements script

--- scripts/conda-install-requirements.sh
+++ scripts/conda-install-requirements.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 conda install pyflakes pip pep8 coverage
-pip install pytest pytest-pep8 pytest-cov pytest-flakes sortedcontainers
+pip install pytest pytest-pep8 pytest-cov pytest-flakes
+pip install https://github.com/eblade/blist/archive/master.zip
