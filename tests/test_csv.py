5a93c916b75a7dcb53530666836e7b81723bf113
Johan Egneblad <johan@egneblad.se>
2016-10-04 23:35:08 +0100

    Make lint happy

--- tests/test_csv.py
+++ tests/test_csv.py
@@ -2,15 +2,16 @@ from jsondb import csv
 
 
 def test_read_csv():
-    with csv.open('tests/data/simple.csv', types=(str,int,int)) as f:
+    with csv.open('tests/data/simple.csv', types=(str, int, int)) as f:
         rows = list(f)
 
     assert rows[0] == {'a': 'A', 'b': 1, 'c': 11}
     assert rows[1] == {'a': 'B', 'b': 2, 'c': 22}
     assert rows[2] == {'a': 'C', 'b': 3, 'c': 33}
 
+
 def test_lookup_by_method():
-    with csv.open('tests/data/simple.csv', types=(str,int,int)) as f:
+    with csv.open('tests/data/simple.csv', types=(str, int, int)) as f:
         lt = csv.LookupTable(f)
 
     lt.index('a', 'b')
@@ -25,7 +26,7 @@ def test_lookup_by_method():
 
 
 def test_lookup_by_argument():
-    with csv.open('tests/data/simple.csv', types=(str,int,int)) as f:
+    with csv.open('tests/data/simple.csv', types=(str, int, int)) as f:
         lt = csv.LookupTable(f, ('a', 'b'), ('b', 'c'))
 
     assert lt['a':'A'] == 1
